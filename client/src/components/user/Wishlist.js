035fe544fcef780c821a8c2385200d410175baae
Haiyang Hong <hong961127@gmail.com>
2020-11-10 18:16:00 -0800

    Add wishlist page

--- client/src/components/user/Wishlist.js
+++ client/src/components/user/Wishlist.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Button, message, Popconfirm } from "antd";
+import { Link } from "react-router-dom";
+import { CloseOutlined } from "@ant-design/icons";
+
+import formatErrorMsg from "../../utils/formatErrorMsg";
+import { removeFromWishlist } from "../../redux/actions/user";
 
 const Wishlist = () => {
-  return <div className="user-wishlist">Wishlist</div>;
+  const dispatch = useDispatch();
+  const { user } = useSelector(({ user }) => user);
+
+  const handleRemoveFromWishlist = async (product) => {
+    try {
+      await dispatch(removeFromWishlist(product._id));
+      message.success(`${product.title} removed from your wishlist.`);
+    } catch (error) {
+      message.error(formatErrorMsg(error), 6);
+    }
+  };
+
+  return (
+    <div className="user-wishlist">
+      {user.wishlist.map((product) => (
+        <div className="user-wishlist__item">
+          {/* Product image */}
+          <img
+            src={product.images[0].url}
+            alt={product.title}
+            className="user-wishlist__item__img"
+          />
+
+          {/* Link to the product */}
+          <div className="user-wishlist__item__content">
+            <Link to={`/products/${product.slug}`}>{product.title}</Link>
+          </div>
+
+          {/* Delete button */}
+          <Popconfirm
+            title={`Remove ${product.title} from wishlist?`}
+            onConfirm={() => handleRemoveFromWishlist(product)}
+          >
+            <Button
+              type="ghost"
+              icon={<CloseOutlined />}
+              size="small"
+              className="user-wishlist__item__close"
+            />
+          </Popconfirm>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Wishlist;
